6720b9e0b48f9bb78d3907c03c0c08bfb2f0df8a
AndreiVomHA <135841134+AndreiVHF7023@users.noreply.github.com>
2023-08-15 17:07:21 +0800

    Add files via upload

---
+++ InvetorySystem/Sys/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index (request):
+    return render(request, "index.html")
+def order (request):
+    return render(request, "order.html")
+def product (request):
+    return render (request, "product.html")
+def staff (request):
+    return render (request, "staff.html")
